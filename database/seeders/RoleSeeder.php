86194a873253d60c3a27460734cc442e5a48f80e
islamabdurahman <abdurahmanislam304@gmail.com>
2023-12-08 10:30:00 +0500

    push

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            [
+                'name' => 'Admin',
+                'guard_name' => 'web',
+            ],
+            // Add more records as needed
+        ]);
+    }
+}
