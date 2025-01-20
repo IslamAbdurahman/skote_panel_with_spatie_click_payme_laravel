46eae6c8b5803c7e0d17acb001f1bb77212f8338
islamabdurahman <abdurahmanislam304@gmail.com>
2025-01-20 13:46:06 +0500

    paginate

--- resources/js/app.js
+++ resources/js/app.js
@@ -201,31 +201,31 @@ File: Main Js File
             $("#dark-mode-switch").prop("checked", false);
             $("#rtl-mode-switch").prop("checked", false);
             $("#dark-rtl-mode-switch").prop("checked", false);
-            $("#bootstrap-style").attr('href', 'assets/css/bootstrap.min.css');
-            $("#app-style").attr('href', 'assets/css/app.min.css');
+            $("#bootstrap-style").attr('href', '/assets/css/bootstrap.min.css');
+            $("#app-style").attr('href', '/assets/css/app.min.css');
             sessionStorage.setItem("is_visited", "light-mode-switch");
         } else if ($("#dark-mode-switch").prop("checked") == true && id === "dark-mode-switch") {
             $("html").removeAttr("dir");
             $("#light-mode-switch").prop("checked", false);
             $("#rtl-mode-switch").prop("checked", false);
             $("#dark-rtl-mode-switch").prop("checked", false);
-            $("#bootstrap-style").attr('href', 'assets/css/bootstrap-dark.min.css');
-            $("#app-style").attr('href', 'assets/css/app-dark.min.css');
+            $("#bootstrap-style").attr('href', '/assets/css/bootstrap-dark.min.css');
+            $("#app-style").attr('href', '/assets/css/app-dark.min.css');
             sessionStorage.setItem("is_visited", "dark-mode-switch");
         } else if ($("#rtl-mode-switch").prop("checked") == true && id === "rtl-mode-switch") {
             $("#light-mode-switch").prop("checked", false);
             $("#dark-mode-switch").prop("checked", false);
             $("#dark-rtl-mode-switch").prop("checked", false);
-            $("#bootstrap-style").attr('href', 'assets/css/bootstrap.rtl.css');
-            $("#app-style").attr('href', 'assets/css/app.rtl.css');
+            $("#bootstrap-style").attr('href', '/assets/css/bootstrap.rtl.css');
+            $("#app-style").attr('href', '/assets/css/app.rtl.css');
             $("html").attr("dir", 'rtl');
             sessionStorage.setItem("is_visited", "rtl-mode-switch");
         } else if ($("#dark-rtl-mode-switch").prop("checked") == true && id === "dark-rtl-mode-switch") {
             $("#light-mode-switch").prop("checked", false);
             $("#rtl-mode-switch").prop("checked", false);
             $("#dark-mode-switch").prop("checked", false);
-            $("#bootstrap-style").attr('href', 'assets/css/bootstrap-dark.rtl.css');
-            $("#app-style").attr('href', 'assets/css/app-dark.rtl.css');
+            $("#bootstrap-style").attr('href', '/assets/css/bootstrap-dark.rtl.css');
+            $("#app-style").attr('href', '/assets/css/app-dark.rtl.css');
             $("html").attr("dir", 'rtl');
             sessionStorage.setItem("is_visited", "dark-rtl-mode-switch");
         }
